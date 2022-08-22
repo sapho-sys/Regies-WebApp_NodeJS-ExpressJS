c4193660295f0c4df3994043ed3d4683b78c0e5b
sapho-sys <saphonkunzi@gmail.com>
2022-08-22 23:17:43 +0200

    Tommorrow

--- routes/router.js
+++ routes/router.js
@@ -16,15 +16,37 @@ let display = displayFactory();
     }
 
     async function homePage(req, res){
-
+        try {
+            await data.populateRegies(req.body.entry);
+            res.redirect('/');
+        } catch (error) {
+            console(error) 
+        }
     }
 
-    async function entryRoute(req, res){
 
+    async function postRegies(req, res){
+        try {
+             display.myTown(req.body.showTown);
+             res.redirect("/filter_regies")  
+        } catch (error) {
+            console.log(error);  
+        }
     }
 
-    async function filterRoute(req, res){
-
+    async function fetchRegies(req,res){
+        try {
+            let code = display.showCode();
+            let myData = [];
+            if(code !== " "){
+                myData = await data.filterRegies(code);
+            }
+          
+            
+        } catch (error) {
+            console.log(error)
+            
+        }
     }
 
     async function resetData(req, res){
@@ -34,8 +56,8 @@ let display = displayFactory();
     return {
         defaultRoute,
         homePage,
-        entryRoute,
-        filterRoute,
+        postRegies,
+        fetchRegies,
         resetData
     }
 
